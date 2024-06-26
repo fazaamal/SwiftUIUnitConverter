90fff13a760d083c54cf996c36e06e910d5f86ab
fazaamal <fazasophian123@gmail.com>
2024-06-26 13:09:03 +0800

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Faza Amal Sophian on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
